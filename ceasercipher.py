0e4499f021a692c46e978630530a2455a8a1daaf
Aditya Babar <70708282+adityababar07@users.noreply.github.com>
2024-02-22 07:50:17 +0000

    initial changes

--- ceasercipher.py
+++ ceasercipher.py
@@ -2,8 +2,26 @@ import random as rd
 
 data = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-cipher = []
+# cipher = rd.shuffle(data)
+cipher = rd.sample(data)
 
-for _ in data:
-    rd.randint(1, 58)
+
+print(cipher)
+
+text = input("Enter your text:\t")
+
+output_text = ""
+
+for i in text:
+    index = data.index(i)
+    print(cipher[index])
+    element = output_text + cipher[index]
+
+print(f"The encrypted text is:-\n {output_text}")
+
+with open("cipher.txt", 'w+') as f:
+    f.write(cipher)
+
+
+
 
