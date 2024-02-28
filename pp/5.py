ddde893615e8e8fb4967de0ff5a7ee98c3dbd753
Aditya Babar <adityababar715@gmail.com>
2024-02-28 10:28:52 +0000

    initial

---
+++ pp/5.py
@@ -0,0 +1,19 @@
+# sum 0f first 100 even no.
+
+sum = 0
+num = 0
+
+num_100 = []
+
+while True:
+    if len(num_100) == 100:
+        for i in num_100:
+            sum += i
+        print(sum)
+        exit()
+
+    num += 1
+
+    if num % 2 == 0:
+        num_100.append(num) 
+
